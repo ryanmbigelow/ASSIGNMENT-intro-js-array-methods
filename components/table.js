eef2ad4d3887e9f97d56034e87151d2868196e5d
ryanmbigelow <ryanmbigelow@gmail.com>
2022-11-01 17:48:17 -0500

    finished the project

--- components/table.js
+++ components/table.js
@@ -5,6 +5,6 @@ export const tableRow = (object) => {
   return `<tr>
     <td>${object.title}</td>
     <td>${object.type}</td>
-    <td>$${object.price.toFixed(2)}</td>
+    <td>$${object.price}</td>
   </tr>`
 }
